9d1388675871ac36f88ab8791da76d8e1d7e7a12
Nidhi <nidhi1997.jsr@gmail.com>
2026-09-23 16:30:50 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [formData, setFormData] = useState({});
+
+  return (
+    <div className="container">
+      <h1>Task Manager System</h1>
+      <form className="form-container">
+        <input type="text" placeholder="Enter title" />
+        <input type="text" placeholder="Enter description" />
+        <select>
+          <option value="pending">Pending</option>
+          <option value="inProgress">In Progress</option>
+          <option value="completed">Completed</option>
+        </select>
+        <select>
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option>
+        </select>
+        <input type="date" placeholder="Enter due Date" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
